f51a9ff6e6ac53ffaebc66ab2729af500a5aba2b
mayspieker <maya.spieker@gmail.com>
2023-06-26 08:20:14 -0300

    inserir finalizado

--- bd2/appbook/app.py
+++ bd2/appbook/app.py
@@ -31,20 +31,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/books", methods=["GET"])
-def books_index():
-    col = get_collection()
-    # res = col.find({"title": "iPhone in Action"})
-    res = col.find(
-        {"title": {"$regex": "iPhone"}}
-    )
-    books = list(res)
-    return render_template("books_list.html", books=books)
-
 @app.route("/questoes/index", methods=["GET"])
 def questoes_index():
     col = get_collection()
-    res = col.find()
+    res = col.find().sort('numQuest')
     questoes = list(res)
     return render_template("questoes_list.html", questoes=questoes)
 
@@ -54,19 +44,56 @@ def questoes_index():
 def questoes_novo():
     return render_template("questoes.html", action='inserir', questoes=None)
 
+@app.route("/questoes/editar/<id>", methods=["GET"])
+def questoes_editar(id):
+    collection = get_collection()
+
+    questoes = list(collection.find({"_id": ObjectId(id)}))
+
+
+    for questao in questoes:
+        ### ALTERNATIVAS ###
+        # Obtenha a lista de alternativas da questão
+        alternativas = questao['alternativas']
+
+        # Extraia apenas os valores 'alt' das alternativas em uma lista separada
+        alternativas_vals = [alt['op'] for alt in alternativas]
+
+        # Junte os valores das alternativas em uma única string, separados por vírgula
+        alternativas_str = ', '.join(alternativas_vals)
+
+        # Adicione a string das alternativas de volta à questão
+        questao['alternativas'] = alternativas_str
+
+        ### TOPICOS ###
+
+        # Obtenha a lista de tópicos da questão
+        topicos = questao['topico']
+
+        # Junte os tópicos em uma única string, separados por vírgula
+        topicos_str = ', '.join(topicos)
+
+        # Adicione a string dos tópicos de volta à questão
+        questao['topico'] = topicos_str
+
+        
+    return render_template("questoes.html", action='editar', questao=questao)
+
 # INSERT = insertOne
 @app.route("/questoes/inserir", methods=["POST"])
 def inserir_questoes():
     col = get_collection()
 
+    # CRIA DE MANEIRA ESTATICA A NUMERAÇAO PRAS ALTEERNATIVAS
     alt = ['a', 'b', 'c', 'd', 'e']
 
+    # PEGA O VALOR INSERIDO NO FORM
     numQuestao = request.form.get('numQuest')
     enunciado = request.form.get('enunciado')
     topicos_str = request.form.get('topico')
     alternativas_str = request.form.get('alternativas')
 
-    # CONFERE SE FOI INSERIDO DADOS E FAZ O SPLIT
+    # CONFERE SE FOI INSERIDO ALGO E SÓ AI FAZ O SPLIT
     if topicos_str and alternativas_str is not None: 
         topicos_list = topicos_str.split(',')
         alternativas_list = alternativas_str.split(',')
@@ -74,18 +101,19 @@ def inserir_questoes():
         alternativas_list = []
         topicos_list = []
 
-    resposta = request.form.get('resposta')
+    resposta = int(request.form.get('resposta'))
 
+    # FAZ UM ARRAY DE ALTERNATIVAS POIS SÃO VÁRIAS ALTERNATIVAS
     alternativas = []
-    for i, alt in enumerate(alternativas_list):
-        alternativa = {"alt": alt[i], "op": alternativas}
+    for i, op in enumerate(alternativas_list):
+        alternativa = {"alt": alt[i], "op": op}
         alternativas.append(alternativa)
 
 
     questao = {
         "numQuest": numQuestao,
         "enunciado": enunciado,
-        "topicos": topicos_list,
+        "topico": topicos_list,
         "alternativas": alternativas,
         "resposta": resposta
     }
@@ -112,9 +140,34 @@ def questoes_delete(id):
     return redirect(url_for('questoes_index'))
 
 # UPDATE = updateOne
-@app.route("/questoes/atualizar", methods=["POST"])
-def questoes_atualizar():
+@app.route("/questoes/edit/", methods=["POST"])
+def questoes_edit():
     collection = get_collection()
+    
+    id = request.form.get('id')
+
+
+    numQuest = request.form.get('numQuest')
+    enunciado = request.form.get('enunciado')
+    topicos = request.form.get('topico')
+    alternativas = request.form.get('alternativas')
+    resposta = request.form.get('resposta')
+
+    result = collection.update_one({"_id": ObjectId(id)}, {"$set": {
+            "numQuest": numQuest,
+            "enunciado": enunciado,
+            "topicos": topicos,
+            "alternativas": alternativas,
+            "resposta": resposta
+        }})
+    
+    if result.modified_count > 0:
+        flash(f'Questão atualizada com sucesso!', 'success')
+    else: 
+        flash(f'Erro ao atualizar questão!', 'danger')
+
+    return redirect(url_for('questoes_index'))
+
 
 
 if __name__ == "__main__":
